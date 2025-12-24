c486a0574ed79cc1d4e8d3b721d712c9e47d6770
BaraaAlmassri <chrollo12388@gmail.com>
2025-12-24 13:10:33 +0200

    UMS

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const getUsers = async () => {
+    const response = await axios.get(`http://ums12.runasp.net/api/users`);
+    return response.data;
+}
+
+const displayUsers = async () => {
+    const result = await getUsers();
+    console.log(result);
+
+    const users  = result.users.map ( (user) => {
+        return `
+
+          <tr> 
+          
+          <td>  <h2> ${user.name} </h2></td>
+          <td>  <img src ="${user.imageUrl}" /></td>
+          <td> <button class="btn btn-outline-danger mt-2" onclick=deleteUser(${user.id})> Delete </button> </td>
+          </tr>
+                 
+        `
+    }).join(' ');
+
+    document.querySelector(".users .users-data").innerHTML = users;
+
+}
+
+displayUsers();
+
+
+const deleteUser = async (id) => {
+
+    const response = await axios.delete(`http://ums12.runasp.net/api/users/${id}`)
+   displayUsers();
+   
+}
+
+ 
+  
